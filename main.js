9b4da193c5cd79aff9184e91000e14f0ffee44ed
Salma Emad <153438569+Salma-Emad-Salem@users.noreply.github.com>
2024-03-23 21:39:13 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const wrapper =document.querySelector('.wrapper');
 const loginLink =document.querySelector('.login-link');
 const registerLink =document.querySelector('.register-link');
+const burgerIcon =document.querySelector('.burger')
+const linksIco =document.querySelector('.links')
+const closemenu =document.querySelector('.closeMenu')
 const btnpopUp =document.querySelector('.btnLogin-popup');
+const btnpopUpmenu =document.querySelector('.btnLogin-popupmenu');
 const iconClose =document.querySelector('.iconClose');
 
 registerLink.addEventListener('click', ()=>{
@@ -13,6 +17,20 @@ loginLink.addEventListener('click', ()=>{
 btnpopUp.addEventListener('click', ()=>{
     wrapper.classList.add('active-popUp')
 })
+burgerIcon.addEventListener('click', ()=>{
+    linksIco.classList.add('memo')
+})
+closemenu.addEventListener('click', ()=>{
+    linksIco.classList.remove('memo')
+})
+btnpopUpmenu.addEventListener('click', ()=>{
+    wrapper.classList.add('active-popUp')
+})
 iconClose.addEventListener('click', ()=>{
     wrapper.classList.remove('active-popUp')
 })
+btnpopUpmenu.addEventListener('', ()=>{
+    wrapper.classList.remove('active-popUp')
+})
+
+
